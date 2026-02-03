9b87d3084f389298e12173bc6314327cfc7eb78c
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2026-02-03 23:48:36 +0900

    [feat] Swagger 문서 도입

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-security-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
